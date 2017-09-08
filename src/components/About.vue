ba7d5e85781e2f14101165a20b4295b945408f31
olshve <olshve@ttu.ee>
2017-09-08 21:44:41 +0300

    [8]

--- src/components/About.vue
+++ src/components/About.vue
@@ -49,8 +49,8 @@ export default {
 			this.y = e.clientY / 4;
 		},
 		mobileChangePerspective(e) {
-			const { beta: y, gamma: x } = e;
-			this.x = -x * 3; this.y = y * 3;
+			const { alpha: y, gamma: x } = e;
+			this.x = -x * 4; this.y = y * 4;
 		}
 	}
 }
