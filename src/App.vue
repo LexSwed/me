9d0a95d5834bc59fb111682e67cb83a44b1e38b3
Oleksandr Shvechykov <lexswed@gmail.com>
2018-02-23 13:39:00 +0200

    some additions

--- src/App.vue
+++ src/App.vue
@@ -13,131 +13,135 @@
 import Navbar from "@/components/Navbar.vue";
 
 export default {
-    data() {
-        return {
-            main: "white",
-            about: "white",
-            showcase: "white"
-        };
-    },
-    mounted() {
-        const docStyle = window.getComputedStyle(this.$el, null);
-        this.main = docStyle.getPropertyValue("--main-accent");
-        this.about = docStyle.getPropertyValue("--about-accent");
-        this.showcase = docStyle.getPropertyValue("--showcase-accent");
-        this.updateColor(this.routeName);
-    },
-    methods: {
-        updateColor(routeName) {
-            const color = this[routeName];
-            document
-                .querySelector("meta[name=theme-color]")
-                .setAttribute("content", color);
-            document.documentElement.style.setProperty("--accent", color);
-        }
-    },
-    computed: {
-        routeName() {
-            return this.$route.name;
-        }
-    },
-    watch: {
-        $route({ name }) {
-            setTimeout(() => {
-                this.updateColor(name);
-            }, 325);
-        }
-    },
-    components: {
-        Navbar
+  data() {
+    return {
+      main: "white",
+      about: "white",
+      showcase: "white"
+    };
+  },
+  mounted() {
+    const docStyle = window.getComputedStyle(this.$el, null);
+    this.main = docStyle.getPropertyValue("--main-accent");
+    this.about = docStyle.getPropertyValue("--about-accent");
+    this.showcase = docStyle.getPropertyValue("--showcase-accent");
+    this.updateColor(this.routeName);
+  },
+  methods: {
+    updateColor(routeName) {
+      const color = this[routeName];
+      document
+        .querySelector("meta[name=theme-color]")
+        .setAttribute("content", color);
+      document.documentElement.style.setProperty("--accent", color);
     }
+  },
+  computed: {
+    routeName() {
+      return this.$route.name;
+    }
+  },
+  watch: {
+    $route({ name }) {
+      setTimeout(() => {
+        this.updateColor(name);
+      }, 325);
+    }
+  },
+  components: {
+    Navbar
+  }
 };
 </script>
 
 <style lang="scss">
 html,
 body {
-    font-family: "Quicksand", sans-serif;
+  font-family: "Quicksand", sans-serif;
 }
 
 body {
-    padding: 0;
-    margin: 0;
+  padding: 0;
+  margin: 0;
 }
 
 :root {
-    --main: #18191c;
-    --main-accent: #f5bc60;
-    --about: #050505;
-    --about-accent: #23cf5f;
-    --showcase: #23263d;
-    --showcase-accent: #e91e63;
-    --light: #fffcff;
-    --text-color: var(--light);
-    --block-shadow: 0 1px 3px rgba(0, 0, 0, 0.4);
-    --b-r: 5px;
-    --accent: var(--main-accent);
-    --dark: rgb(12, 12, 12);
-    --warning: #cf5c36;
+  --main: #18191c;
+  --main-accent: #f5bc60;
+  --about: #050505;
+  --about-accent: #23cf5f;
+  --showcase: #23263d;
+  --showcase-accent: #e91e63;
+  --light: #fffcff;
+  --text-color: var(--light);
+  --block-shadow: 0 1px 3px rgba(0, 0, 0, 0.4);
+  --b-r: 5px;
+  --accent: var(--main-accent);
+  --dark: rgb(12, 12, 12);
+  --warning: #cf5c36;
 }
 
 body * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 h2,
 h3,
 h4 {
-    margin: 10px 0;
+  margin: 10px 0;
 }
 
 #app {
-    height: 100vh;
-    width: 100vw;
-    max-height: 100%;
-    overflow-y: auto;
-    overflow-y: overlay;
-    transition: background-color 0.3s 0.2s;
-    background-color: #18191c;
-    position: relative;
-    &::-webkit-scrollbar {
-        display: none;
-    }
-    *::selection {
-        background: transparent;
-        color: var(--accent);
-    }
+  height: 100vh;
+  width: 100vw;
+  max-height: 100%;
+  overflow-y: auto;
+  overflow-y: overlay;
+  transition: background-color 0.3s 0.2s;
+  background-color: #18191c;
+  position: relative;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  *::selection {
+    background: transparent;
+    color: var(--accent);
+  }
+}
+
+.about-accent {
+  color: var(--about-accent);
 }
 
 .main {
+  position: relative;
+  top: 15vh;
+  max-width: 900px;
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  transition: filter 1s;
+  border-radius: var(--b-r);
+  > section {
     position: relative;
-    top: 15vh;
-    max-width: 900px;
-    width: 100%;
-    margin-left: auto;
-    margin-right: auto;
-    transition: filter 1s;
-    border-radius: var(--b-r);
-    > section {
-        position: relative;
-    }
-    * {
-        perspective: 300px;
-    }
+  }
+  * {
+    perspective: 300px;
+  }
 }
 
 @keyframes fade {
-    from {
-        opacity: 0;
-    }
-    to {
-        opacity: 1;
-    }
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 }
 @media screen and (max-width: 600px) {
-    .main {
-        top: 12vh;
-        padding-bottom: 50px;
-    }
+  .main {
+    top: 12vh;
+    padding-bottom: 50px;
+  }
 }
 </style>
--- src/components/About.vue
+++ src/components/About.vue
@@ -2,14 +2,15 @@
 	<section class="about">
         <header>
             <p>
-                My real name is <span style="color: var(--about-accent)">Oleksandr Shvechykov</span>.
-                I am from <span style="color: var(--about-accent)">Ukraine</span> but currently I study in <span style="color: var(--about-accent)">Tartu</span>.
-                I love front end a lot, but I was taught <span style="color: var(--about-accent)">much more</span>.
+                Blessed saints! I am so glad <span class="about-accent">you</span> are here!<br>
+                My real name is <span class="about-accent">Oleksandr Shvechykov</span>.
+                I am from <span  class="about-accent">Ukraine</span> but currently I study in <span class="about-accent">Tartu</span>.
+                I love front end a lot, but I was taught <span class="about-accent">much more</span>.
             </p>
         </header>
         <div class="s-card">
             <header>What you can ask me for:</header>
-            <ul class="skills-grid">
+            <ul class="skills-grid" v-dragscroll>
                 <li v-for="s in $options.skills" :key="s.name">
                     <div class="s-name">{{ s.name }}</div>
                     <div class="s-short">{{s.short}}</div>
@@ -20,168 +21,171 @@
 </template>
 
 <script>
+import { dragscroll } from "vue-dragscroll";
+
 export default {
-    skills: [
-        {
-            name: "HTML / CSS",
-            short:
-                "Confident in layout creation and styling. Use a lot on daily basis."
-        },
-        {
-            name: "Node.js",
-            short: "Have a not long experience in creating API using Node.js."
-        },
-        {
-            name: "Java",
-            short: "I wrote some Java for some university courses."
-        },
-        {
-            name: "Elixir",
-            short:
-                "We built some simple API for our university project, using it with Phoenix for quick setup of routers, controllers and model. I like it."
-        },
-        {
-            name: "React",
-            short:
-                "One of the main tools. I have a good experience rewriting a project from jQuery to React. I always keep an eye on latest news."
-        },
-        {
-            name: "Vue.js",
-            short:
-                "Main tool for creating front end applications. Good experience in implementing a project from zero to deployment."
-        },
-        {
-            name: "F#",
-            short:
-                "I have learnt some basics for the `Advanced programming` course. Love it the most from all FP languages I tried."
-        },
-        {
-            name: "R",
-            short:
-                "I have used it a lot for the Data Mining course (not because I love it, just because we had a lot of homeworks)."
-        }
-    ],
-    data() {
-        return {
-            active: 0
-        };
+  skills: [
+    {
+      name: "HTML / CSS",
+      short:
+        "Confident in layout creation and styling. Use HTML, CSS (SCSS actually) a lot on daily basis."
+    },
+    {
+      name: "Node.js",
+      short: "Have a not long experience in creating API using Node.js."
+    },
+    {
+      name: "Java",
+      short: "I wrote some Java for some university courses."
+    },
+    {
+      name: "Elixir",
+      short:
+        "We built some simple API for our university project, using it with Phoenix for quick setup of routers, controllers and model. I like it."
+    },
+    {
+      name: "React",
+      short:
+        "One of the main tools. I have a good experience rewriting a project from jQuery to React. I always keep an eye on latest news."
+    },
+    {
+      name: "Vue.js",
+      short:
+        "Main tool for creating front end applications. Good experience in implementing a project from zero to deployment."
+    },
+    {
+      name: "F#",
+      short:
+        "I have learnt some basics for the `Advanced programming` course. Love it the most from all FP languages I tried."
+    },
+    {
+      name: "R",
+      short:
+        "I have used it a lot for the Data Mining course (not because I love it, just because we had a lot of homeworks)."
     }
+  ],
+  data() {
+    return {
+      active: 0
+    };
+  },
+  directives: { dragscroll }
 };
 </script>
 
 <style lang="scss">
 .about {
-    color: var(--text-color);
-    min-height: 600px;
-    top: -5vh;
-    > header {
-        width: 100%;
-        position: relative;
-        padding: 0 60px;
-        margin-bottom: 40px;
-        p {
-            font-size: 24px;
-            padding: 0;
-            margin: 0;
-        }
+  color: var(--text-color);
+  min-height: 600px;
+  top: -5vh;
+  > header {
+    width: 100%;
+    position: relative;
+    padding: 0 60px;
+    margin-bottom: 40px;
+    p {
+      font-size: 24px;
+      padding: 0;
+      margin: 0;
     }
+  }
 }
 .s-card {
-    transition: box-shadow 0.4s, transform 0.4s;
+  transition: box-shadow 0.4s, transform 0.4s;
 }
 .skills-grid {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-gap: 8px;
-    list-style: none;
-    padding: 0;
-    margin: 0;
-    overflow: auto;
-    li {
-        height: 100px;
-        display: flex;
-        flex-flow: column nowrap;
-        justify-content: space-between;
-        overflow: hidden;
-        box-shadow: 0 0 4px rgba(0, 0, 0, 0.6);
-        transition: transform 0.4s, opacity 0.2s, border-color 0.1s;
-        background: rgba(255, 255, 255, 0.1);
-        border: 1px solid transparent;
-        min-width: 210px;
-        &:before {
-            content: "";
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background: radial-gradient(rgba(255, 255, 255, 0.1), var(--about));
-            transition: opacity 0.4s;
-            opacity: 0.2;
-        }
-        &:hover {
-            border-color: var(--about-accent);
-            &:before {
-                opacity: 0.5;
-            }
-            .s-name {
-                opacity: 0;
-                transform: translateY(-10%);
-            }
-            .s-short {
-                opacity: 1;
-                transform: translateY(-100%);
-            }
-        }
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 8px;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+  li {
+    height: 100px;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: space-between;
+    overflow: hidden;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.6);
+    transition: transform 0.4s, opacity 0.2s, border-color 0.1s;
+    background: rgba(255, 255, 255, 0.1);
+    border: 1px solid transparent;
+    min-width: 210px;
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: radial-gradient(rgba(255, 255, 255, 0.1), var(--about));
+      transition: opacity 0.4s;
+      opacity: 0.2;
     }
+    &:hover {
+      border-color: var(--about-accent);
+      &:before {
+        opacity: 0.5;
+      }
+      .s-name {
+        opacity: 0;
+        transform: translateY(-10%);
+      }
+      .s-short {
+        opacity: 1;
+        transform: translateY(-100%);
+      }
+    }
+  }
 }
 .s-name {
-    font-size: 18px;
-    padding: 41px 20px;
-    text-align: center;
-    line-height: 18px;
-    transition: opacity 0.3s, transform 0.3s;
+  font-size: 18px;
+  padding: 41px 20px;
+  text-align: center;
+  line-height: 18px;
+  transition: opacity 0.3s, transform 0.3s;
 }
 .s-short {
-    padding: 4px;
-    opacity: 0;
-    font-size: 14px;
-    transition: opacity 0.3s, transform 0.3s;
+  padding: 4px;
+  opacity: 0;
+  font-size: 14px;
+  transition: opacity 0.3s, transform 0.3s;
 }
 .about-enter,
 .about-leave-to {
+  opacity: 0;
+  > header {
+    border-color: white;
+    p {
+      opacity: 0;
+      filter: grayscale(1);
+    }
+  }
+  .skills-grid > li {
     opacity: 0;
-    > header {
-        border-color: white;
-        p {
-            opacity: 0;
-            filter: grayscale(1);
-        }
-    }
-    .skills-grid > li {
-        opacity: 0;
-        transform: translate3d(-20px, 50px, 0);
-    }
+    transform: translate3d(-20px, 50px, 0);
+  }
 }
 .about-leave-active,
 .about-enter-active {
-    transition: background-color 0.7s, opacity 0.3s;
-    > header {
-        transition: border-color 0.7s;
-        p {
-            transition: opacity 0.3s, filter 0.7s 0.2s;
-        }
+  transition: background-color 0.7s, opacity 0.3s;
+  > header {
+    transition: border-color 0.7s;
+    p {
+      transition: opacity 0.3s, filter 0.7s 0.2s;
     }
-    @for $i from 1 through 8 {
-        .skills-grid > li:nth-child(#{$i}) {
-            transition-delay: #{$i * 0.06}s;
-        }
+  }
+  @for $i from 1 through 8 {
+    .skills-grid > li:nth-child(#{$i}) {
+      transition-delay: #{$i * 0.06}s;
     }
+  }
 }
 @media screen and (max-width: 600px) {
-    .about {
-        padding: 0 1rem;
-        perspective: 2000px;
-    }
+  .about {
+    padding: 0 1rem;
+    perspective: 2000px;
+  }
 }
 </style>
