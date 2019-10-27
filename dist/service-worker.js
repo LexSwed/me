e0deb8408f4b607691d5371be10272b231dd0d06
LexSwed <oleksandr.shvechykov@pipedrive.com>
2019-10-27 17:06:38 +0200

    release new build

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dist/precache-manifest.9dec0f9a09d9c743c8b9231676d4675b.js"
+  "/dist/precache-manifest.66a2f77f93a8856772df97d07b893f3a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "lexswed.github.io"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "lexswed.github.io"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
