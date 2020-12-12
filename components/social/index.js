256e558a74cc6cf8d6688f9926694ab250a1cf77
Alexander Swed <lexswed@gmail.com>
2020-12-12 10:32:42 +0100

    migrate to nuxt beacuse I can

---
+++ components/social/index.js
@@ -0,0 +1,15 @@
+import Dev from './dev.vue';
+import Facebook from './facebook.vue';
+import GitHub from './github.vue';
+import GitLab from './gitlab.vue';
+import LinkedIn from './linkedin.vue';
+import Twitter from './twitter.vue';
+
+export default {
+  Dev,
+  Facebook,
+  GitHub,
+  GitLab,
+  LinkedIn,
+  Twitter
+};
