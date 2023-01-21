dd5d1b9ffff093db6d30d0078b557ca3593ca026
LexSwed <lexswed@gmail.com>
2023-01-21 21:57:38 +0100

    small style fixes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,13 +6,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "background": "#232946", // just dark
-        "on-background": "#fffffe",
+        // "background": "#232946", // just dark
+        // "on-background": "#fffffe",
         // "background": "#b8c1ec", // blueish
         // "on-background": "#232946",
-        // "primary": "#8EC1D6", // blue-ish
-        "primary": "#FDBA74", // yellow-ish
+        "background": "#16161a", // blue-dark
+        "on-background": "#fffffe",
+        // "primary": "#8EC1D6", // aqua-ish
+        // "primary": "#FDBA74", // yellow-ish
         // "primary": "#eebbc3", // pink-ish
+        "primary": "#b8c1ec", // blue-ish
         "on-primary": "#232946",
         "stroke": "#010101",
       },
