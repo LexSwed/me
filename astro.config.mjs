e4790399a62c8420e34841de72d29a5cff791737
LexSwed <lexswed@gmail.com>
2023-01-27 18:03:19 +0100

    visual updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 import vercel from "@astrojs/vercel/static";
+import commonjs from "vite-plugin-commonjs";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,4 +12,7 @@ export default defineConfig({
   integrations: [mdx(), sitemap(), tailwind(), prefetch()],
   output: "static",
   adapter: vercel(),
+  vite: {
+    plugins: [commonjs()],
+  },
 });
