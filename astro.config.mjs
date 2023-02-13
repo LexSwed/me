7e51613e6f64e7d57b51a000ddc2d4dbc320c484
LexSwed <lexswed@gmail.com>
2023-02-13 09:06:44 +0100

    revert back to static mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel";
 import commonjs from "vite-plugin-commonjs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind(), prefetch()],
-  output: "server",
+  output: "static",
   adapter: vercel({
     analytics: true,
   }),
