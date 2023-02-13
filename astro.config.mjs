51c825e687685533229d97b068073a8bc0c5aaac
LexSwed <lexswed@gmail.com>
2023-02-13 18:34:29 +0100

    server

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 import commonjs from "vite-plugin-commonjs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind(), prefetch()],
-  output: "static",
+  output: "server",
   adapter: vercel({
     analytics: true,
   }),
