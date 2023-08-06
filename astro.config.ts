e56ef40908b6e8eb17684a274942df39f050a750
LexSwed <lexswed@gmail.com>
2023-08-06 17:49:16 +0200

    return vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import prefetch from "@astrojs/prefetch";
-import preact from "@astrojs/preact";
-import { remarkShiki } from "./plugins/shiki";
-import cloudflare from "@astrojs/cloudflare";
-// const shikiResourcePaths = Object.keys(import.meta.glob(["./node_modules/shiki/languages/*.json", "./node_modules/shiki/themes/material-palenight.json"]));
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import prefetch from '@astrojs/prefetch';
+import preact from '@astrojs/preact';
+import { remarkShiki } from './plugins/shiki';
+const shikiResourcePaths = Object.keys(
+  import.meta.glob([
+    './node_modules/shiki/languages/*.json',
+    './node_modules/shiki/themes/material-palenight.json',
+  ]),
+);
+
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alvechy.vercel.app",
+  site: 'https://alvechy.vercel.app',
   markdown: {
     syntaxHighlight: false,
     gfm: true,
@@ -18,16 +24,19 @@ export default defineConfig({
       [
         remarkShiki,
         {
-          theme: "material-theme-palenight",
+          theme: 'material-theme-palenight',
         },
       ],
     ],
   },
   integrations: [mdx(), sitemap(), tailwind(), prefetch(), preact()],
-  output: "hybrid",
+  output: 'hybrid',
   experimental: {
     assets: true,
     viewTransitions: true,
   },
-  adapter: cloudflare(),
+  adapter: vercel({
+    analytics: true,
+    includeFiles: shikiResourcePaths,
+  }),
 });
