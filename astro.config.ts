64a25640bfbd7b5a34c5947aa4686a553e1adc25
LexSwed <lexswed@gmail.com>
2023-09-02 11:09:13 +0200

    try to use cloudflare image service

--- astro.config.ts
+++ astro.config.ts
@@ -23,6 +23,11 @@ export default defineConfig({
       ],
     ],
   },
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/noop",
+    },
+  },
   integrations: [mdx(), sitemap(), tailwind(), prefetch(), preact()],
   output: "hybrid",
   adapter: cloudflare({
