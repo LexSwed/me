3d848936cb52c7e41d5c6bddfca0f7ecbcd140f9
LexSwed <lexswed@gmail.com>
2024-12-08 19:01:40 +0100

    remove some animations, upgrade to astro 5

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
 import robotsTxt from "astro-robots-txt";
 
 import expressiveCode from "astro-expressive-code";
@@ -39,8 +38,5 @@ export default defineConfig({
     tailwind(),
     robotsTxt(),
   ],
-  output: "hybrid",
-  adapter: cloudflare({
-    imageService: "compile",
-  }),
+  output: "static",
 });
