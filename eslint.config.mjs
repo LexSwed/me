d7784ec0cae377d193364d8a1454224412600686
LexSwed <lexswed@gmail.com>
2025-02-01 16:11:32 +0100

    eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,60 +1,11 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import parser from "astro-eslint-parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename) + "/src";
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import eslintPluginAstro from "eslint-plugin-astro";
+import tseslint from "typescript-eslint";
 
 export default [
   {
     ignores: ["**/*.cjs", "**/.obsidian/**"],
   },
-  ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:astro/recommended",
-  ),
-  {
-    plugins: {
-      "@typescript-eslint": typescriptEslint,
-    },
-
-    languageOptions: {
-      globals: {
-        astroHTML: "readonly",
-      },
-    },
-  },
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-
-    languageOptions: {
-      parser: tsParser,
-    },
-  },
-  {
-    files: ["**/*.astro"],
-
-    languageOptions: {
-      parser: parser,
-      ecmaVersion: 5,
-      sourceType: "script",
-
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
-      },
-    },
-
-    rules: {},
-  },
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs["flat/jsx-a11y-recommended"],
 ];
--- tailwind.config.cjs
+++
@@ -1,7 +0,0 @@
-/**
- * Temporary for Tailwind VS Code extension
- * @type {import('tailwindcss').Config}
- */
-module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-};
